5c831a9c88cc221abdc29e097509b5ef568b9215
Saagcode <146259869+Saagcode@users.noreply.github.com>
2025-05-17 09:17:45 -0300

    alteracoes logo

--- components/central-call-btn/index.jsx
+++ components/central-call-btn/index.jsx
@@ -4,7 +4,10 @@ function CentralCallBtn() {
   return (
     <>
       <div className="container-centralCallBtn">
-        <button className="content-centralCallBtn">
+        <button
+          className="content-centralCallBtn"
+          onClick={() => window.open(`https://www.lojatintasfischer.com.br`)}
+        >
           <h1>COMPRAR ON-LINE</h1>
         </button>
       </div>
